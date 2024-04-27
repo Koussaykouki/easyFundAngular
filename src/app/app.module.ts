ef9ab130db476c06758d19bab1d51f829783f228
Koussaykouki <koussay.kouki@esprit.tn>
2024-04-27 23:31:54 +0100

    claimservices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,24 +10,16 @@ import { AllTemplateBackComponent } from './BackOffice/all-template-back/all-tem
 import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
 
 import { OffreComponent } from './BackOffice/offre/offre.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { OfferService  } from './services/offer.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { OfferService } from './services/offer.service';
 import { HttpClientModule } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Uncomment if you are using animations
 import { FinancingsComponent } from './BackOffice/financings/financings.component';
-import { MatDialogModule } from '@angular/material/dialog';
+// import { MatDialogModule } from '@angular/material/dialog'; // Uncomment if using MatDialog
 import { ExcelComponent } from './BackOffice/excel/excel.component';
-import { FormBuilder, FormGroup, Validators,FormsModule } from '@angular/forms';
-import { saveAs } from 'file-saver';
 
 import { LoginComponent } from './BackOffice/login/login.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { RegistrationComponent } from './BackOffice/registration/registration.component';
-import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
-
-
-
 
 @NgModule({
   declarations: [
@@ -37,33 +29,24 @@ import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsMod
     SidebarBackComponent,
     AllTemplateBackComponent,
     AllTemplateFrontComponent,
-
     OffreComponent,
     FinancingsComponent,
-    ExcelComponent
-    
-
+    ExcelComponent,
     LoginComponent,
     RegistrationComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatDialogModule,
-    FormsModule
-    
+    // BrowserAnimationsModule, // Uncomment if using animations
+    // MatDialogModule, // Uncomment if using MatDialog
   ],
   providers: [
-    provideAnimationsAsync()
-
-    FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule
-
+    OfferService // Example of a service you might have
+    // Add other services here
   ],
   bootstrap: [AppComponent]
 })
